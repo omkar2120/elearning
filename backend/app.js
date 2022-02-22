57223b4c32ef6d2e5f0f8b79d34d5d74ac6e65af
anshika056 <anshikamadan2@gmail.com>
2022-02-22 14:01:25 +0530

    server setup

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config({path:"./config.env"});
+const PORT=process.env.PORT ;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/",(req,res)=>{
+    res.status(200).json("hello there!");
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running at ${PORT}`);
+})
