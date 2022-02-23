8864d1e2a1e8f6964ecbc80adeb3b06e46c1614d
omkar2120 <naradomkar212000@gmail.com>
2022-02-23 17:02:54 +0530

    home navbar done

--- frontend/src/global/component/Navbar.js
+++ frontend/src/global/component/Navbar.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Container, CssBaseline, Divider } from '@mui/material';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+         <Box sx={{ flexGrow: 1 }}>
+         <CssBaseline/>
+           <AppBar position="fixed" color='inherit'  sx={{boxShadow:0, justifySelf:'flex-start'}}>
+             <Toolbar>
+               <Typography variant="h6" sx={{ flexGrow: 3 }}>
+                ELearning
+              </Typography>
+              <Toolbar sx={{flexGrow:15, justifyContent:'space-evenly'}}>
+                <Typography>Hello</Typography>
+                <Typography>Hello</Typography>
+                <Typography>Hello</Typography>
+              </Toolbar>
+                 
+              <Divider orientation="vertical" variant="middle" flexItem sx={{height:'20px', marginTop:'23px'}} />
+
+             <Toolbar sx={{ justifySelf:'flex-end'}}>
+               <Button color="inherit" variant='outlined' sx={{borderRadius:7 , marginRight:'10px'}}>Teacher Login</Button>
+               <Button variant='contained' sx={{borderRadius:7 , color:'#fafafa'}}>Student Login </Button>
+             </Toolbar>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
