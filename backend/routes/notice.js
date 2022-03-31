ddf6c50f496313188125901301c86bd8240ca2f0
anshika056 <anshikamadan2@gmail.com>
2022-03-31 23:04:55 +0530

    changes

--- backend/routes/notice.js
+++ backend/routes/notice.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createnotice, getnotice, updatenotice, delnot } = require("../controllers/notice");
+const { AdminAndTeacherMiddleware } = require("../middleware");
 const router = express.Router();
 
 
-router.post("/createnotice",createnotice);
+router.post("/createnotice",AdminAndTeacherMiddleware,createnotice);
 
-router.get("/getnotices",getnotice);
+router.get("/getnotices",AdminAndTeacherMiddleware,getnotice);
 
 router.put("/updatenotice/:id",updatenotice)
 
